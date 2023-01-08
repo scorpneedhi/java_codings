321d4ceabdc755927dca521bb3c3695a24bcf76e
Needhi Devan Senthil Kumar <needhi.senthil@gmail.com>
2023-01-08 11:42:32 -0700

    287

---
+++ p287.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set= new HashSet<Integer>();
+        for(int num:nums){
+            if(set.contains(num))
+                return num;
+            set.add(num);
+        }
+        return 0;
+    }
+}
