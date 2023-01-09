f913bcbd2e91c11f746b118b12016f7081f0f8ba
Needhi Devan Senthil Kumar <needhi.senthil@gmail.com>
2023-01-09 16:22:59 -0700

    27-new

--- 27.java
+++ 27.java
@@ -1,16 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int index =0;
-        Arrays.sort(nums);
         for(int i =0; i<nums.length; i++)
         {
-            if(nums[i]==val)
-                nums[i]=nums[nums.length-1]+1;
+            if(nums[i]!=val)
+                nums[i-index]=nums[i];
             else
                 ++index;
         }
-        Arrays.sort(nums);
-        return index;
+        return nums.length-index;
 
     }
 }
